6d6cff4eed89a3435831eceb98d3bb712e7fe394
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2023-05-10 11:49:38 +0300

    create logout

--- logout/index.php
+++ logout/index.php
@@ -7,5 +7,5 @@ if (!empty($_SESSION['login'])) {
 if (isset($_SERVER['HTTP_REFERER'])) {
     header('Location: ' . $_SERVER['HTTP_REFERER']);
 } else {
-    header('Location: ../index.php');
+    header('Location: ../');
 }
