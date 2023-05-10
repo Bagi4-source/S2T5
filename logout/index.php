368b99b2bc609425e7338d17f85ed85dbbd86766
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2023-05-10 22:01:56 +0300

    update login form

--- logout/index.php
+++ logout/index.php
@@ -1,10 +1,11 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
-$actual_link = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-header('Referer: ' . $actual_link);
-http_response_code(302);
 
 session_start();
 session_destroy();
 
-http_redirect('../');
+if (isset($_SERVER['HTTP_REFERER'])) {
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+} else {
+    header('Location: ../');
+}
