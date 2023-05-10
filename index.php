6c17cd05e60df24ea81ab7f775dcb0eb83740f59
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2023-05-10 12:29:45 +0300

    create logout

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
 <?php
-$a = mail("german.bagdasaryan@mail.ru", "Регистрация", "Ваш логин: лавщфта");
-print($a);
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require_once '/PHPMailer/src/Exception.php';
+require_once '/PHPMailer/src/PHPMailer.php';
+require_once '/PHPMailer/src/SMTP.php';
+
+// Для более ранних версий PHPMailer
+//require_once '/PHPMailer/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+$mail->CharSet = 'UTF-8';
+
+// Настройки SMTP
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+$mail->SMTPDebug = 0;
+
+$mail->Host = 'ssl://smtp.gmail.com';
+$mail->Port = 465;
+$mail->Username = 'nubasik331@gmail.com';
+$mail->Password = 'NubikMain';
+
+// От кого
+$mail->setFrom('mail@snipp.ru', 'Snipp.ru');
+
+// Кому
+$mail->addAddress('german.bagdasaryan@mail.ru', 'Иван Петров');
+
+// Тема письма
+$mail->Subject = "Регистрация";
+
+// Тело письма
+$body = '<p><strong>«Hello, world!» </strong></p>';
+$mail->msgHTML($body);
+
+$mail->send();
