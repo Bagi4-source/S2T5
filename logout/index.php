703ca206592b6dddf4402f143076f40010d7953c
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2023-05-10 22:00:06 +0300

    update login form

--- logout/index.php
+++ logout/index.php
@@ -7,8 +7,4 @@ http_response_code(302);
 session_start();
 session_destroy();
 
-if (isset($_SERVER['HTTP_REFERER'])) {
-    header('Location: ' . $_SERVER['HTTP_REFERER']);
-} else {
-    header('Location: ../');
-}
+http_redirect('../');
