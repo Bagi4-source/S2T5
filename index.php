f4fb56f1d3111f4c41778e7e37bbc4baba116525
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2023-05-10 19:16:39 +0300

    create logout

--- index.php
+++ index.php
@@ -5,31 +5,40 @@ require './phpMailer/Exception.php';
 
 // Для более ранних версий PHPMailer
 //require_once '/PHPMailer/PHPMailerAutoload.php';
-
-$mail = new PHPMailer\PHPMailer\PHPMailer;
-$mail->CharSet = 'UTF-8';
+function sendMessage($body)
+{
+    $mail = new PHPMailer\PHPMailer\PHPMailer;
+    $mail->CharSet = 'UTF-8';
 
 // Настройки SMTP
-$mail->isSMTP();
-$mail->SMTPAuth = true;
-$mail->SMTPDebug = 0;
+    $mail->isSMTP();
+    $mail->SMTPAuth = true;
+    $mail->SMTPDebug = 0;
 
-$mail->Host = 'ssl://smtp.rambler.ru';
-$mail->Port = 465;
-$mail->Username = 'keitzaharova36757@rambler.ru';
-$mail->Password = 'adUZ742Qpfgu';
+    $mail->Host = 'ssl://smtp.rambler.ru';
+    $mail->Port = 465;
+    $mail->Username = 'keitzaharova36757@rambler.ru';
+    $mail->Password = 'adUZ742Qpfgu';
 
 // От кого
-$mail->setFrom('keitzaharova36757@rambler.ru', 'Snipp.ru');
+    $mail->setFrom('keitzaharova36757@rambler.ru', 'Bagi4');
 
 // Кому
-$mail->addAddress('german.bagdasaryan@mail.ru', 'Иван Петров');
+    $mail->addAddress('german.bagdasaryan@mail.ru');
 
 // Тема письма
-$mail->Subject = "тестовое письмо";
+    $mail->Subject = "Регистрация";
 
 // Тело письма
-$body = '<p><strong>«Hello, world!» </strong></p>';
-$mail->msgHTML($body);
+    $mail->msgHTML($body);
 
-$mail->send();
+    $mail->send();
+
+}
+
+$login = 'afafaf';
+$password = 'password';
+$regText = sprintf('<p><strong>Вы успешно зарегистрировались!</strong></p>
+<p>Логин: %s</p>
+<p>Пароль: %s</p>', $login, $password);
+sendMessage($regText);
